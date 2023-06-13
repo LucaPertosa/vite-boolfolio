621ec9b568d465ca4392b202815d3c44235ab6b6
LucaPertosa <luca.pertosa@hotmail.it>
2023-06-13 19:22:24 +0200

    Create vue project + Card component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+    data () {
+        return {
+            projects: [],
+            baseUrl: "http://127.0.0.1:8002",
+        }; 
+
+    },
+    mounted () {
+        this.getProjects();
+    },
+    methods: {
+        getProjects () {
+            axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+                this.projects = resp.data.results
+                console.log(this.projects);
+            });
+        }
+    },
+    components: {
+        ProjectCard,
+    }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="container">
+        <h1 class="text-center my-3">Lista dei progetti</h1>
+        <div class="row row-cols-4 g-3">
+            <div class="col" v-for="project in projects" :key="project.id">
+                <ProjectCard :project="project"/>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+    @use "./styles/general.scss" as *;
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8002",
+        }
+    }   
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" :alt="project.title" class="card-img-top">
+        <div v-else class="my-3">
+            <p class="text-center">Nessuna immagine presente per questo progetto</p>
+        </div>
+        <div class="card-body">
+            <h5 class="text-center">{{ project.title }}</h5>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>

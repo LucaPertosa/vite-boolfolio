4312d075b20843bf921ad81f133a4f8520790524
LucaPertosa <luca.pertosa@hotmail.it>
2023-06-14 15:29:36 +0200

    Add store.js + page loader

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <script>
 import axios from 'axios';
 import ProjectCard from './components/ProjectCard.vue';
+import { store } from './store';
 
 export default {
     data () {
         return {
+            store,
             projects: [],
-            baseUrl: "http://127.0.0.1:8002",
+            currentPage: 1,
+            lastPage: null,
+            totalProjects: 0,
+            load: false,
         }; 
-
     },
     mounted () {
         this.getProjects();
     },
     methods: {
-        getProjects () {
-            axios.get(`${this.baseUrl}/api/projects`).then(resp => {
-                this.projects = resp.data.results
-                console.log(this.projects);
+        getProjects (pageNuber = 1) {
+            this.load = true;
+            axios.get(`${this.store.apiBaseUrl}/api/projects`, {
+                params: {
+                    page: pageNuber,
+                }
+            }).then(resp => {
+                console.log(resp);
+                this.projects = resp.data.results.data;
+                this.currentPage = resp.data.results.current_page;
+                this.lastPage = resp.data.results.last_page;
+                this.totalProjects = resp.data.results.total;
+            }).finally(() => {
+                this.load = false;
             });
         }
     },
@@ -30,11 +44,23 @@ export default {
 <template>
     <div class="container">
         <h1 class="text-center my-3">Lista dei progetti</h1>
-        <div class="row row-cols-4 g-3">
-            <div class="col" v-for="project in projects" :key="project.id">
-                <ProjectCard :project="project"/>
+        <section v-if="!load">
+            <div class="text-end text-secondary my-3">
+                Trovati {{ totalProjects }} Progetti
             </div>
-        </div>
+            <div class="row row-cols-4 g-3">
+                <div class="col" v-for="project in projects" :key="project.id">
+                    <ProjectCard :project="project"/>
+                </div>
+            </div>
+            
+        </section>
+        <section v-else>
+            <div class="d-flex align-items-center justify-content-center">
+                <strong class="text-info">Loading...</strong>
+                <div class="spinner-border text-info ms-3" role="status" aria-hidden="true"></div>
+            </div>
+        </section>
     </div>
 </template>
 
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,4 +1,6 @@
 <script>
+import { store } from '../store';
+
 export default {
     name: 'ProjectCard',
     props: {
@@ -6,20 +8,27 @@ export default {
     },
     data() {
         return {
-            baseUrl: "http://127.0.0.1:8002",
+            store,
         }
-    }   
+    },
+    computed: {
+        // crea una funzione che mi gestisce l'url da inserire nell'src dell immagine
+        getImgUrl() {
+            return this.store.apiBaseUrl + '/storage/' + this.project.image;
+        }
+    }  
 }
 </script>
 
 <template>
     <div class="card h-100">
-        <img v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" :alt="project.title" class="card-img-top">
+        <img v-if="project.image" :src="getImgUrl" :alt="project.title" class="card-img-top">
         <div v-else class="my-3">
             <p class="text-center">Nessuna immagine presente per questo progetto</p>
         </div>
         <div class="card-body">
             <h5 class="text-center">{{ project.title }}</h5>
+
         </div>
     </div>
 </template>
